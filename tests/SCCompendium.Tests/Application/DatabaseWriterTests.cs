a452489f25917399b58aa2eba1855df579ec8bfa
9thAzure <dl6741@yahoo.ca>
2026-01-27 21:04:31 -0500

    Added initial SQL writing

---
+++ tests/SCCompendium.Tests/Application/DatabaseWriterTests.cs
@@ -0,0 +1,65 @@
+using System.Data.Common;
+using Apps72.Dev.Data.DbMocker;
+using SCCompendium.Application;
+using SCCompendium.Domain;
+
+namespace SCCompendium.Tests.Application;
+
+public class DatabaseWriterTests
+{
+    public class CommandCapturer
+    {
+        public string? CommandText { get; private set; }
+        public IEnumerable<DbParameter>? Parameters { get; private set; }
+        public bool HasCapturedMultipleCommands { get; private set; }
+        public bool Capture(MockCommand command)
+        {
+            if (CommandText is not null)
+            {
+                HasCapturedMultipleCommands = true;
+            }
+            CommandText = command.CommandText;
+            Parameters = command.Parameters;
+            return true;
+        }
+    }
+
+    [Test]
+    public async Task WriteSections_SampleSource_CorrectWriteAndCount()
+    {
+        const string sampleTableName = "abcdef";
+        string longCredit = new ('d', 100);
+        string longTitle = new string('e', 100);
+        DatabaseWriter writer = new();
+        Dictionary<string, (string, List<PhonologicalRule>)> source = new()
+        {
+            {"", ("", null!)},
+            {"aaa", ("bbb", null!)},
+            {"ccc", (longCredit, [])},
+            {longTitle, ("fff", [default, default])}
+        };
+        MockDbConnection connection = new();
+        CommandCapturer capturer = new();
+        connection.Mocks
+            .HasValidSqlServerCommandText()
+            .When(capturer.Capture)
+            .ReturnsScalar(source.Count);
+
+       int modifiedCount = writer.WriteSections(source, connection, sampleTableName);
+
+       await Assert.That(capturer).IsNotNull();
+       await Assert.That(modifiedCount).IsEqualTo(source.Count);
+       string[] capturedCommandParts = capturer.CommandText.Split(' ', StringSplitOptions.TrimEntries);
+       await Assert.That(capturedCommandParts[0]).IsEqualTo("INSERT", StringComparison.CurrentCultureIgnoreCase);
+       await Assert.That(capturedCommandParts[1]).IsEqualTo("INTO", StringComparison.CurrentCultureIgnoreCase);
+       await Assert.That(capturedCommandParts[2]).IsEqualTo(sampleTableName);
+       await Assert.That(capturer.CommandText).Contains("VALUE", StringComparison.CurrentCultureIgnoreCase);
+       await Assert.That(capturer.Parameters)
+           .Contains(p => "aaa".Equals((string)p.Value!, StringComparison.CurrentCultureIgnoreCase))
+           .And.Contains(p => "bbb".Equals((string)p.Value!, StringComparison.CurrentCultureIgnoreCase))
+           .And.Contains(p => "ccc".Equals((string)p.Value!, StringComparison.CurrentCultureIgnoreCase))
+           .And.Contains(p => longTitle.Equals((string)p.Value!, StringComparison.CurrentCultureIgnoreCase))
+           .And.Contains(p => longCredit.Equals((string)p.Value!, StringComparison.CurrentCultureIgnoreCase))
+           .And.Contains(p => "fff".Equals((string)p.Value!, StringComparison.CurrentCultureIgnoreCase));
+    }
+}
