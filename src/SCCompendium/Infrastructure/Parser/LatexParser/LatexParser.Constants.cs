441eab49bdc4deded836263c8ba4f753edfda68d
9thAzure <dl6741@yahoo.ca>
2026-05-01 22:17:46 -0400

    Added hashset for characters escaped by "

--- src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Constants.cs
+++ src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Constants.cs
@@ -7,6 +7,7 @@ public partial class LatexParser
 {
     private const char TipaIgnoreNextChar = (char)26; // 'Substitute' character, used as it seems unused and thematically similar.
 
+    private static readonly HashSet<char> _escapedChars = new("#$&%{}");
     private const string TipaInput  = ":;\"0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ|";
     private const string TipaOutput = "ː\u02D1ˈʉɨʌɜɥɐɒɤɵɘəɑβɕðɛɸɣɦɪʝʁʎɱŋɔʕɾʃθʊʋɯχʏʒ|";
     private static readonly Dictionary<char, char> _tipaSingleCharConversions =
