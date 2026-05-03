2756524d8c2c92c9a4dc5bb417da4a5b0118a7f0
9thAzure <dl6741@yahoo.ca>
2026-05-02 21:39:55 -0400

    Reworked Context class

--- src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Context.cs
+++ src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Context.cs
@@ -6,9 +6,39 @@ namespace SCCompendium.Infrastructure.Parser.LatexParser;
 public partial class LatexParser
 {
     private delegate ReadOnlySpan<char> Command(ReadOnlySpan<char> remaining, Context context);
-    private record struct Context(StringBuilder Result,
-        Dictionary<string, Command> Commands)
+    private class Context
     {
+        public StringBuilder Source { get; } = new();
+        public StringBuilder Result { get; } = new();
+        public required Dictionary<string, Command> CommandList { get; set; }
 
+        public Context(ReadOnlySpan<char> initialSource)
+        {
+            Source.Append(initialSource);
+        }
+
+        public char PeekSource(int index = 0)
+        {
+            if (index >= Source.Length)
+            {
+                return '\0';
+            }
+
+            return Source[index];
+        }
+
+        public char PopSource()
+        {
+            char c = Source[0];
+            Source.Remove(0, 1);
+            return c;
+        }
+
+        public char ConsumeSource()
+        {
+            char c = PopSource();
+            Result.Append(c);
+            return c;
+        }
     }
 }
