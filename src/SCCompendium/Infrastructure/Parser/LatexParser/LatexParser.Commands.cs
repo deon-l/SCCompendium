65d503a78773f47352f38430d46c29bcdee546ca
9thAzure <dl6741@yahoo.ca>
2026-05-06 22:36:05 -0400

    recreated and hookedup ipa command

---
+++ src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Commands.cs
@@ -0,0 +1,88 @@
+namespace SCCompendium.Infrastructure.Parser.LatexParser;
+
+public partial class LatexParser
+{
+    private static void CommandTextIpa(Context context)
+    {
+        int baseDepth = context.GroupDepth;
+        do
+        {
+            char c = context.PeekSource();
+            if (c == TipaIgnoreNextChar)
+            {
+                _ = context.PopSource();
+                c = context.PeekSource();
+                if (c == '\\')
+                {
+                    Debug.Assert(_escapedChars.Contains(context.PeekSource(1)));
+                    context.ConsumeSource();
+                    context.ConsumeSource();
+                    continue;
+                }
+
+                context.ConsumeSource();
+            }
+            ParseCharacter(context);
+        } while (context.GroupDepth > baseDepth);
+    }
+
+    // private static void CommandSuper(Context context)
+    // {
+    //     int baseDepth = context.GroupDepth;
+    //     do
+    //     {
+    //         context.Result.Append(argument[i] switch
+    //         {
+    //             'h' => 'ʰ',
+    //             'l' => 'ˡ',
+    //             'm' => 'ᵐ',
+    //             'n' => 'ⁿ',
+    //             'j' => 'ʲ',
+    //             'w' => 'ʷ',
+    //             'x' => 'ˣ',
+    //             'y' => 'ʸ',
+    //             // This command is TIPA exclusive, so the capital conversions are preemptively applied.
+    //             'H' => 'ʱ',
+    //             'M' => 'ᶬ',
+    //             'N' => 'ᵑ',
+    //             'P' => 'ˀ',
+    //             'Q' => 'ˤ',
+    //             'W' => 'ᵚ',
+    //             _ => throw new ArgumentException(
+    //                 $"Cannot raise '{argument[i]}' (limitation of encoding or not implemented)", nameof(segment))
+    //         });
+    //     } while (context.GroupDepth > baseDepth);
+    //
+    //     return segment;
+    // }
+
+    // private static ReadOnlySpan<char> CommandAsterisk(ReadOnlySpan<char> segment, Context context)
+    // {
+    //     StringBuilder argument = new();
+    //     segment = GetArgument(segment, context, argument);
+    //
+    //     for (int i = 0; i < argument.Length; i++)
+    //     {
+    //         context.Result.Append(argument[i] switch
+    //         {
+    //             'f' => 'ⅎ',
+    //             'k' => 'ʞ',
+    //             'r' => 'ɹ',
+    //             't' => 'ʇ',
+    //             'w' => 'ʍ',
+    //             'j' => 'ɟ',
+    //             'n' => 'ɲ',
+    //             'h' => 'ħ',
+    //             'l' => 'ɬ',
+    //             'z' => 'ɮ',
+    //             _ => TipaIgnoreNextChar
+    //         });
+    //         if (context.Result[^1] == TipaIgnoreNextChar)
+    //         {
+    //             context.Result.Append(argument[i]);
+    //         }
+    //     }
+    //
+    //     return segment;
+    // }
+}
--- src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Constants.cs
+++ src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Constants.cs
@@ -12,10 +12,24 @@ public partial class LatexParser
 
     private const string TipaInput  = ":;\"0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ|";
     private const string TipaOutput = "ː\u02D1ˈʉɨʌɜɥɐɒɤɵɘəɑβɕðɛɸɣɦɪʝʁʎɱŋɔʕɾʃθʊʋɯχʏʒ|";
-    private static readonly Dictionary<char, char> _tipaSingleCharConversions =
-        Enumerable.Zip(TipaInput, TipaOutput).ToDictionary();
+
+    private static readonly Dictionary<char, string> _tipaSingleCharConversions =
+        Enumerable.Zip(TipaInput, TipaOutput)
+            .Select(pair => (pair.First, pair.Second.ToString()))
+            .ToDictionary();
 
     private static readonly Dictionary<string, CommandData> _normalCommands = new();
     private static readonly Dictionary<string, CommandData> _tipaCommands = new();
     private static readonly Dictionary<string, CommandData> _mathCommands = new();
+
+    static LatexParser()
+    {
+        _normalCommands.Add("ipa", _commandTextIpaData);
+        _normalCommands.Add("textipa", _commandTextIpaData);
+    }
+
+    private static readonly CommandData _commandTextIpaData = new(1, CommandTextIpa,
+        new Typeset(_tipaCommands,
+            new() { { ('|', '|'), "‖" }, {('\"', '\"'), "ˌ"} },
+            _tipaSingleCharConversions));
 }
