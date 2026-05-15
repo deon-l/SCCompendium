8a38bdbf4278358b6c10b9fd83e64cd366cf7993
9thAzure <dl6741@yahoo.ca>
2026-05-14 21:04:18 -0400

    Fixed bf command not consuming an extra whitespace

--- src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Commands.cs
+++ src/SCCompendium/Infrastructure/Parser/LatexParser/LatexParser.Commands.cs
@@ -18,10 +18,12 @@ public partial class LatexParser
     }
 
 
-    private static readonly CommandData _commandBfData = new(0, _nullCommand, new(null, null,
+    private static readonly CommandData _commandBfData = new(0,
+        context => _ = Char.IsWhiteSpace(context.PeekSource()) ? context.PopSource() : '\0',
+        new(null, null,
             CreateReplacements("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
                 "𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳")
-            ),false);
+        ), false);
 
     private static void CommandTextIpa(Context context)
     {
