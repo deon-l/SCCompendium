931791269c2da47277d69b0c8c27002e51c6fb69
9thAzure <dl6741@yahoo.ca>
2026-06-15 14:51:48 -0400

    Use ToString of listed rules instead

--- src/SCCompendium/Domain/PhonologicalRuleGroup.cs
+++ src/SCCompendium/Domain/PhonologicalRuleGroup.cs
@@ -3,5 +3,5 @@ namespace SCCompendium.Domain;
 public record struct PhonologicalRuleGroup(string Title, string Credit, List<PhonologicalRule> Rules)
 {
     public override string ToString()
-        => $"{Title} - {Credit}\n\t- {String.Join("\n\t- ", Rules.Select(rule => rule.Rule))}";
+        => $"{Title} - {Credit}\n\t- {String.Join("\n\t- ", Rules.Select(rule => rule.ToString()))}";
 }
