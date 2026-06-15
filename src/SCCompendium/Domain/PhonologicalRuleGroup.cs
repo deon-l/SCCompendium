d5b92489410ef68e8131bbadc447ba1237d9e393
9thAzure <dl6741@yahoo.ca>
2026-06-15 14:50:27 -0400

    Add toString override to `PhonologicalRuleGroup`

--- src/SCCompendium/Domain/PhonologicalRuleGroup.cs
+++ src/SCCompendium/Domain/PhonologicalRuleGroup.cs
@@ -1,3 +1,7 @@
 namespace SCCompendium.Domain;
 
-public record struct PhonologicalRuleGroup(string Title, string Credit, List<PhonologicalRule> Rules);
+public record struct PhonologicalRuleGroup(string Title, string Credit, List<PhonologicalRule> Rules)
+{
+    public override string ToString()
+        => $"{Title} - {Credit}\n\t- {String.Join("\n\t- ", Rules.Select(rule => rule.Rule))}";
+}
