9bb30b81b616174252df01cb857019b0820068e5
9thAzure <dl6741@yahoo.ca>
2026-06-15 14:54:44 -0400

    added documentation

--- src/SCCompendium/Domain/PhonologicalRuleGroup.cs
+++ src/SCCompendium/Domain/PhonologicalRuleGroup.cs
@@ -2,6 +2,7 @@ namespace SCCompendium.Domain;
 
 public record struct PhonologicalRuleGroup(string Title, string Credit, List<PhonologicalRule> Rules)
 {
+    /// <summary>Returns semi-formated string of all data contained in this instance.</summary>
     public override string ToString()
         => $"{Title} - {Credit}\n\t- {String.Join("\n\t- ", Rules.Select(rule => rule.ToString()))}";
 }
