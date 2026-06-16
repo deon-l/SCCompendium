4180347886d378baec930c9aa013e851f7c031df
Deon Liu <deon.liu@outlook.com>
2026-06-15 21:28:00 -0400

    Add various Unit Tests for `DiachronicaParser` (#21)
    * Updated multiple section tests to be even more robust
    * Added incomplete test for sections with notes
    * Added tests checking for correct afterapplication of notes
    * Fixed warnings of possible null dereference

--- tests/SCCompendium.Tests/Infrastructure/Parser/DiachronicaParserTests.cs
+++ tests/SCCompendium.Tests/Infrastructure/Parser/DiachronicaParserTests.cs
@@ -89,8 +89,8 @@ V\ipa{b} \textrightarrow\ V / \ipa{y}_ \\
         var latexParser = MockableILatexParser.Mock();
         latexParser.ParseLatexSegment(Any(), Any()).Callback((str, sb) => sb.Append(">"+str));
         var ruleParser = IPhonologicalRuleParser.Mock();
-        var arg1 = Is<string>(s => s.Contains('x'));
-        var arg2 = Is<string>(s => s.Contains('y'));
+        var arg1 = Is<string>(s => s?.Contains('x') ?? false);
+        var arg2 = Is<string>(s => s?.Contains('y') ?? false);
         ruleParser.TryParseRule(arg1).Returns(true).SetsOutRule(_defaultRule);
         ruleParser.TryParseRule(arg2).Returns(true).SetsOutRule(_defaultRule);
         DiachronicaParser parser = new(latexParser.Object, ruleParser.Object);
@@ -112,24 +112,24 @@ V\ipa{b} \textrightarrow\ V / \ipa{y}_ \\
 """
 \section{s1}
 \ipa{a} \change\ \ipa{a}
-\subsection{s2}
+\subsection{s2}cr2
 \ipa{a} \change\ \ipa{a}
-\subsubsection{s3}
+\subsubsection{s3}cr3
 \ipa{a} \change\ \ipa{a}
 \section{s4}
 \ipa{a} \change\ \ipa{a}
-\subsection{s5}
+\subsection{s5}cr5
 \ipa{a} \change\ \ipa{a}
-\paragraph{s6}
+\paragraph{s6}cr6
 \ipa{a} \change\ \ipa{a}
-\subparagraph{s7}
+\subparagraph{s7}cr7
 \ipa{a} \change\ \ipa{a}
 
 """;
         var latexParser = MockableILatexParser.Mock();
         latexParser.ParseLatexSegment(Any(), Any()).Callback((str, sb) => sb.Append(">"+str));
         var ruleParser = IPhonologicalRuleParser.Mock();
-        var arg1 = Is<string>(s => s.Contains("ipa{a}"));
+        var arg1 = Is<string>(s => s?.Contains("ipa{a}") ?? false);
         ruleParser.TryParseRule(arg1).Returns(true).SetsOutRule(_defaultRule);
         DiachronicaParser parser = new(latexParser.Object, ruleParser.Object);
 
@@ -145,7 +145,159 @@ V\ipa{b} \textrightarrow\ V / \ipa{y}_ \\
         await Assert.That(result[2].Title).IsEqualTo(">s5");
         await Assert.That(result[3].Title).IsEqualTo(">s6");
         await Assert.That(result[4].Title).IsEqualTo(">s7");
+        await Assert.That(result[0].Credit).StartsWith(">cr2");
+        await Assert.That(result[1].Credit).StartsWith(">cr3");
+        await Assert.That(result[2].Credit).StartsWith(">cr5");
+        await Assert.That(result[3].Credit).StartsWith(">cr6");
+        await Assert.That(result[4].Credit).StartsWith(">cr7");
         await Assert.That(arg1.Values).Count().IsEqualTo(5);
+    }
+
+    [Test]
+    public async Task Parse_SectionWithNotaBene_TakesAsNote()
+    {
+        const string input =
+"""
+\section{s1}
+\subsection{s2}
+NB: note 1 (Don't think its used this way.)
+\ipa{z} \change\ \ipa{z}
+\subsection{s3}
+\tab {\it NB: expected format}
+\ipa{z} \change\ \ipa{z}
+\subsection{s4}
+\tab \textit{NB: this is used once}
+\ipa{z} \change\ \ipa{z}
+""";
+        var latexParser = MockableILatexParser.Mock();
+        latexParser.ParseLatexSegment(Any(), Any()).Callback((str, sb) => sb.Append(str));
+        var ruleParser = IPhonologicalRuleParser.Mock();
+        ruleParser.TryParseRule(str => str.Contains('z')).Returns(true).SetsOutRule(_defaultRule);
+        DiachronicaParser parser = new(latexParser.Object, ruleParser.Object);
+
+        var result = parser.Parse(new StringReader(input));
+
+        await Assert.That(result).Count().IsEqualTo(3);
+        foreach (PhonologicalRuleGroup group in result)
+        {
+            await Assert.That(group.Rules).Count().IsEqualTo(1);
+        }
+        throw new NotImplementedException("PhonologicalRuleGroup does not have field for note yet");
+    }
+
+    [Test]
+    public async Task Parse_HasNonNote_NotUsedAsNote()
+    {
+        const string input =
+            """
+            \section{s1}
+            \subsection{s2}
+            \ipa{z} \change\ \ipa{z}\\
+            Some miscellaneous thing that isn't a note involving \ipa{a} and stuff.\\
+            \ipa{z} \change\ \ipa{z}\\
+            \ipa{z} \change\ \ipa{z}
+            """;
+        var latexParser = MockableILatexParser.Mock();
+        latexParser.ParseLatexSegment(Any(), Any()).Callback((str, sb) => sb.Append(str));
+        var ruleParser = IPhonologicalRuleParser.Mock();
+        ruleParser.TryParseRule(str => str.Contains('z')).Returns(true).SetsOutRule(_defaultRule);
+        DiachronicaParser parser = new(latexParser.Object, ruleParser.Object);
+
+        var result = parser.Parse(new StringReader(input));
+
+        await Assert.That(result).Count().IsEqualTo(1);
+        await Assert.That(result[0].Rules).Count().IsEqualTo(3);
+        await Assert.That(result[0].Rules).DoesNotContain(rule => !String.IsNullOrWhiteSpace(rule.Note));
+    }
+
+    [Test]
+    public async Task Parse_HasNoteWithExplicitApplication_DoesNotOverApply()
+    {
+        const string input =
+            """
+            \section{s1}
+            \subsection{s2}
+            \ipa{z} \change\ \ipa{z}\\
+            Initials:\\
+            ---\ipa{z} \change\ \ipa{z}\\
+            ---\ipa{z} \change\ \ipa{z}\\
+            \ipa{z} \change\ \ipa{z}
+            """;
+        var latexParser = MockableILatexParser.Mock();
+        latexParser.ParseLatexSegment(Any(), Any()).Callback((str, sb) => sb.Append(str));
+        var ruleParser = IPhonologicalRuleParser.Mock();
+        // mock arg matching seem to work in reverse order.
+        ruleParser.TryParseRule(str => str.Contains('z')).Returns(true).SetsOutRule(_defaultRule);
+        ruleParser.TryParseRule(str => str.StartsWith("---")).Returns(true).SetsOutRule(_defaultRule);
+        DiachronicaParser parser = new(latexParser.Object, ruleParser.Object);
+
+        var result = parser.Parse(new StringReader(input));
+
+        await Assert.That(result).Count().IsEqualTo(1);
+        await Assert.That(result[0].Rules).Count().IsEqualTo(4);
+        await Assert.That(result[0].Rules[0].Note).IsEmpty();
+        await Assert.That(result[0].Rules[1].Note).IsNotEmpty();
+        await Assert.That(result[0].Rules[2].Note).IsNotEmpty();
+        await Assert.That(result[0].Rules[3].Note).IsEmpty();
+    }
+
+    [Test]
+    public async Task Parse_HasNoteWithTrickyExplicitApplication_DoesNotOverApplyOrUseWrongNote()
+    {
+        const string input =
+            """
+            \section{s1}
+            \subsection{s2}
+            \ipa{z} \change\ \ipa{z}\\
+            Initials:\\
+            ---not note but does end in w/ a colon (:
+            ---\ipa{z} \change\ \ipa{z} ``ends in a colon (:\\
+            ---\ipa{z} \change\ \ipa{z}\\
+            \ipa{z} \change\ \ipa{z}
+            """;
+        var latexParser = MockableILatexParser.Mock();
+        latexParser.ParseLatexSegment(Any(), Any()).Callback((str, sb) => sb.Append(str));
+        var ruleParser = IPhonologicalRuleParser.Mock();
+        // mock arg matching seem to work in reverse order.
+        ruleParser.TryParseRule(str => str.Contains('z')).Returns(true).SetsOutRule(_defaultRule);
+        ruleParser.TryParseRule(str => str.StartsWith("---\\")).Returns(true).SetsOutRule(_defaultRule);
+        DiachronicaParser parser = new(latexParser.Object, ruleParser.Object);
+
+        var result = parser.Parse(new StringReader(input));
+
+        await Assert.That(result).Count().IsEqualTo(1);
+        await Assert.That(result[0].Rules).Count().IsEqualTo(4);
+        await Assert.That(result[0].Rules[0].Note).IsEmpty();
+        await Assert.That(result[0].Rules[1].Note).StartsWith("Initials:");
+        await Assert.That(result[0].Rules[2].Note).StartsWith("Initials:");
+        await Assert.That(result[0].Rules[3].Note).IsEmpty();
+    }
+
+    [Test]
+    public async Task Parse_HasGreedyNote_TakesAll()
+    {
+        const string input =
+            """
+            \section{s1}
+            \subsection{s2}
+            Initials:\\
+            \ipa{z} \change\ \ipa{z}\\
+            \ipa{z} \change\ \ipa{z}\\
+            \ipa{z} \change\ \ipa{z}
+            """;
+        var latexParser = MockableILatexParser.Mock();
+        latexParser.ParseLatexSegment(Any(), Any()).Callback((str, sb) => sb.Append(str));
+        var ruleParser = IPhonologicalRuleParser.Mock();
+        ruleParser.TryParseRule(str => str.Contains('z')).Returns(true).SetsOutRule(_defaultRule);
+        DiachronicaParser parser = new(latexParser.Object, ruleParser.Object);
+
+        var result = parser.Parse(new StringReader(input));
+
+        await Assert.That(result).Count().IsEqualTo(1);
+        await Assert.That(result[0].Rules).Count().IsEqualTo(3);
+        await Assert.That(result[0].Rules[0].Note).StartsWith("Initials:");
+        await Assert.That(result[0].Rules[1].Note).StartsWith("Initials:");
+        await Assert.That(result[0].Rules[2].Note).StartsWith("Initials:");
     }
 
     [Test]
