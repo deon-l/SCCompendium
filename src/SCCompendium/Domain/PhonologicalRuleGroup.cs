dc33437c6fb4bfdec562778d09879dd748d0922d
Deon Liu <deon.liu@outlook.com>
2026-06-16 11:50:35 -0400

    Add field `Note` to `PhonologicalRuleGroup`, and update appropriate test (#22)
    * Added Note to `PhonologicalRuleGroup` and ToString
    * Updated test that needed to use said field

--- src/SCCompendium/Domain/PhonologicalRuleGroup.cs
+++ src/SCCompendium/Domain/PhonologicalRuleGroup.cs
@@ -1,8 +1,8 @@
 namespace SCCompendium.Domain;
 
-public record struct PhonologicalRuleGroup(string Title, string Credit, List<PhonologicalRule> Rules)
+public record struct PhonologicalRuleGroup(string Title, string Credit, List<PhonologicalRule> Rules, string Note = "")
 {
     /// <summary>Returns semi-formated string of all data contained in this instance.</summary>
     public override string ToString()
-        => $"{Title} - {Credit}\n\t- {String.Join("\n\t- ", Rules.Select(rule => rule.ToString()))}";
+        => $"{Title} - {Credit}\n  ({Note})\n\t- {String.Join("\n\t- ", Rules.Select(rule => rule.ToString()))}";
 }
