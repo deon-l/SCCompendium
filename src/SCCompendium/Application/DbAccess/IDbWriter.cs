a73d9875f7323477933106e854cd65cb98bda589
Deon Liu <deon.liu@outlook.com>
2026-06-17 20:41:11 -0400

    Update interfaces in `Application/DbAccess` (#27)
    * Added IDbSettingsManager
    * Updated IDbWriter

--- src/SCCompendium/Application/DbAccess/IDbWriter.cs
+++ src/SCCompendium/Application/DbAccess/IDbWriter.cs
@@ -1,6 +1,21 @@
-namespace SCCompendium.Application.DBAccess;
+using System.Data;
+using SCCompendium.Domain;
 
+namespace SCCompendium.Application.DbAccess;
+
+/// <summary>
+/// Class for values to Database.
+/// </summary>
 public interface IDbWriter
 {
+    /// <summary>
+    /// Initiate the database with the structure required for storing data (e.g. create tables for relational DBs),
+    /// if such structures don't already exist.
+    /// </summary>
+    public void InitiateDatabase(IDbConnection connection, IDbSettingsManager settings);
 
+    /// <summary>
+    /// Write the entirety of <paramref name="rules"/> into the database.
+    /// </summary>
+    public void Write(IDbConnection connection, IDbSettingsManager settings, List<PhonologicalRuleGroup> rules);
 }
