50d837a25001917d679ac6ada1e2f3c13514ee0d
9thAzure <dl6741@yahoo.ca>
2026-06-18 19:44:12 -0400

    Remove DbAccess interfaces for settings

--- src/SCCompendium/Application/DbAccess/IDbWriter.cs
+++ src/SCCompendium/Application/DbAccess/IDbWriter.cs
@@ -12,10 +12,10 @@ public interface IDbWriter
     /// Initiate the database with the structure required for storing data (e.g. create tables for relational DBs),
     /// if such structures don't already exist.
     /// </summary>
-    public void InitiateDatabase(IDbConnection connection, IDbSettingsManager settings);
+    public void InitiateDatabase(IDbConnection connection);
 
     /// <summary>
     /// Write the entirety of <paramref name="rules"/> into the database.
     /// </summary>
-    public void Write(IDbConnection connection, IDbSettingsManager settings, List<PhonologicalRuleGroup> rules);
+    public void Write(IDbConnection connection, List<PhonologicalRuleGroup> rules);
 }
