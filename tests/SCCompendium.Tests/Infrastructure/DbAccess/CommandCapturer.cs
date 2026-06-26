b8c1e63e05976c1d182c97b2e00c2b73cd5ce1c3
Deon Liu <deon.liu@outlook.com>
2026-06-25 21:53:26 -0400

    Improve `CommandCapturer` (#37)
    * feat: Added `CommandCapturer` as an proper (internal) class, and added enhancements
    * feat: Removed subclass version of `CommandCapturer`, and updated usages accordingly.

---
+++ tests/SCCompendium.Tests/Infrastructure/DbAccess/CommandCapturer.cs
@@ -0,0 +1,36 @@
+using Apps72.Dev.Data.DbMocker;
+
+namespace SCCompendium.Tests.Infrastructure.DbAccess;
+
+internal class CommandCapturer
+{
+    private readonly List<MockCommand> _captured = new();
+    public MockCommand this[int i] => _captured[i];
+    public int Count => _captured.Count;
+    /// <summary>
+    /// Captures all commands, and allows all SQL commands.
+    /// </summary>
+    public Func<MockCommand, bool> Any => CaptureAll;
+
+    private bool CaptureAll(MockCommand command)
+    {
+        _captured.Add(command);
+        return true;
+    }
+
+    /// <summary>
+    /// Allows only SQL commands that satisfy <paramref name="predicate"/>, and captures only if they do.
+    /// </summary>
+    public Func<MockCommand, bool> If(Func<MockCommand, bool> predicate)
+    {
+        return (command) =>
+        {
+            if (!predicate(command))
+            {
+                return false;
+            }
+            _captured.Add(command);
+            return true;
+        };
+    }
+}
