fe38ebaa61bf1c59d3c42196c4bed3767eb21452
9thAzure <dl6741@yahoo.ca>
2026-06-30 14:24:46 -0400

    feat: Added `IDbConnectionRepository`

---
+++ src/SCCompendium/Application/DbAccess/IDbConnectionRepository.cs
@@ -0,0 +1,20 @@
+using System.Data;
+
+namespace SCCompendium.Application.DbAccess;
+
+/// <summary>
+/// Used to generate a <see cref="IDbConnection"/> from a connection string,
+/// and to be passed to other interfaces so that they can get said connection.
+/// </summary>
+public interface IDbConnectionRepository : IDisposable
+{
+    /// <summary>
+    /// Use <paramref name="connectionString"/> to create an <see cref="IDbConnection"/>.
+    /// </summary>
+    public void CreateConnection(string connectionString);
+
+    /// <summary>
+    /// Get an <see cref="IDbConnection"/> instance, or a specified subclass of it.
+    /// </summary>
+    public T GetConnection<T>() where T : IDbConnection;
+}
