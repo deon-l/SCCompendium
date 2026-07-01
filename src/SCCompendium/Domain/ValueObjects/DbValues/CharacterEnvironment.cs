a53b9b566e5a025c36e46403f19250dfdfc700ab
Deon Liu <deon.liu@outlook.com>
2026-06-30 22:34:18 -0400

    Add `IDbReader` and required value objects. (#50)
    * feat: Added Domain value types
    * feat: Added `IDbReader`

---
+++ src/SCCompendium/Domain/ValueObjects/DbValues/CharacterEnvironment.cs
@@ -0,0 +1,13 @@
+namespace SCCompendium.Tests.Domain.ValueObjects.DbValues;
+
+/// <summary>
+/// Represents placement in a phonological rule.
+/// </summary>
+[Flags]
+public enum CharacterEnvironment
+{
+    Input   = 0b_001,
+    Output  = 0b_010,
+    Context = 0b_100,
+    All = Input | Output | Context
+}
