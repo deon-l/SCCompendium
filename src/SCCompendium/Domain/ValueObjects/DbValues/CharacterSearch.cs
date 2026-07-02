76d491eb1319c301a642c0ad22847bb67aa41875
Deon Liu <deon.liu@outlook.com>
2026-07-02 14:32:45 -0400

    Impliment Tests for `DbReader` (#51)
    * test (`DbReader`): Added tests for `DbReader`
    * fix (`Domain.ValueTypes`): Improved equality to perform value equality of collections
    * test: fix incorrect column name in `FindRules` test

--- src/SCCompendium/Domain/ValueObjects/DbValues/CharacterSearch.cs
+++ src/SCCompendium/Domain/ValueObjects/DbValues/CharacterSearch.cs
@@ -5,7 +5,18 @@ public record struct CharacterSearch()
     /// <summary>The character to search for.</summary>
     public string Character { get; init; }
     /// <summary>The diacritics any characters found must have.</summary>
-    public string[] Diacritics { get; init; }
+    public string[] Diacritics { get; init; } = [];
     /// <summary>Specific rule environment the character is found in.</summary>
     public CharacterEnvironment Environment { get; init; } = CharacterEnvironment.All;
+
+    public bool Equals(CharacterSearch other)
+        => Character == other.Character
+           && Environment == other.Environment
+           && Diacritics.Length == other.Diacritics.Length
+           && Diacritics.All(other.Diacritics.Contains);
+
+    public readonly override int GetHashCode()
+    {
+        return HashCode.Combine(Character, Diacritics.Length, (int)Environment);
+    }
 }
---
+++ tests/SCCompendium.Tests/Infrastructure/DbAccess/DbReaderTests.cs
@@ -0,0 +1,183 @@
+using System.Data.Common;
+using Apps72.Dev.Data.DbMocker;
+using SCCompendium.Application.DbAccess;
+using SCCompendium.Domain.ValueObjects.Parsed;
+using SCCompendium.Infrastructure.DbAccess;
+using SCCompendium.Tests.Domain.ValueObjects.DbValues;
+using SCCompendium.Tests.Infrastructure.DbAccess.Helper;
+
+namespace SCCompendium.Tests.Infrastructure.DbAccess;
+
+public class DbReaderTests
+{
+    [Test]
+    public async Task GetAllGroupDetails_SampleDb_ReturnsAllGroups()
+    {
+        BetterMockDbConnection connection = new();
+        CommandCapturer capturer = new();
+        DbNames n = new();
+        var repo = IDbConnectionRepository.Mock();
+        repo.GetConnection<DbConnection>().Returns(connection);
+        PhonologicalRuleGroup g1 = new("Group 1", "Credit 1", [], "Note 1");
+        PhonologicalRuleGroup g2 = new("Group 2", "Credit 2", [], "Note 2");
+        PhonologicalRuleGroup g3 = new("A to B", "\thttps://asdf", []);
+        connection.Mocks
+            .HasValidSqlServerCommandText()
+            .When(capturer.Any)
+            .ReturnsTable(MockTable.WithColumns(n.RuleGroupColKey, n.RuleGroupColName, n.RuleGroupColCredit, n.RuleGroupColNote)
+                .AddRow(1, g1.Title, g1.Credit, g1.Note)
+                .AddRow(2, g2.Title, g2.Credit, g2.Note)
+                .AddRow(1024, g3.Title, g3.Credit, g3.Note));
+        DbReader reader = new();
+
+        var result = reader.GetAllGroupDetails(repo);
+
+        await Assert.That(result).Count().IsEqualTo(3);
+        foreach (var expectedGroup in (PhonologicalRuleGroup[])[g1, g2, g3])
+        {
+            await Assert.That(result).Contains(expectedGroup);
+        }
+    }
+
+    [Test]
+    public async Task GetAllCharacterDetails_SampleDb_ReturnsAllCharacters()
+    {
+        BetterMockDbConnection connection = new();
+        DbNames n = new();
+        var repo = IDbConnectionRepository.Mock();
+        repo.GetConnection<DbConnection>().Returns(connection);
+        connection.Mocks
+            .HasValidSqlServerCommandText()
+            .WhenAny()
+            .ReturnsTable(MockTable.WithColumns(n.IpaCharacterColKey, n.IpaCharacterColSymbol, n.IpaCharacterColDiacritics)
+                .AddRow(0, "a", "")
+                .AddRow(1, "b", ":")
+                .AddRow(11, "ch", ":;[+high]"));
+        IpaCharacter[] expectedChars = [new("a", []), new("b", [":"]), new("ch", [":", "[+high]"])];
+        DbReader reader = new();
+
+        var results = reader.GetAllCharacterDetails(repo);
+
+        await Assert.That(results).Count().IsEqualTo(3);
+        foreach (IpaCharacter expectedChar in expectedChars)
+        {
+            await Assert.That(results).Contains(expectedChar);
+        }
+    }
+
+    [Test]
+    public async Task FindRules_NoFilter_ReturnsAllRules()
+    {
+        BetterMockDbConnection connection = new() { HasValidSqlServerCommandText = true };
+        var repo = IDbConnectionRepository.Mock();
+        repo.GetConnection<DbConnection>().Returns(connection);
+        IpaCharacter
+            charA = new("a", []),
+            charB = new("b", [":"]),
+            charC = new("c", [":", "[+high]"]);
+        List<PhonologicalRuleGroup> g =
+        [
+            new("English to French", "credit 1", [
+                new("a > b:", [charA], [charB], []),
+                new("a > c:[+high] / b:_", [charA], [charC], [charB]),
+            ]),
+            new("English to French", "credit2", Note: "NB: hi", Rules:[
+                new("ab > cc", [charA, charB], [charC], [], "note")
+            ])
+        ];
+        DbNames n = new();
+        connection.Mocks
+            .When(cmd => cmd.CommandTextStartsWith(["SELECT"]) && cmd.SymbolAtContains(3, n.RuleGroupTable))
+            .ReturnsTable(MockTable
+                .WithColumns(n.RuleGroupColKey, n.RuleGroupColName, n.RuleGroupColCredit, n.RuleGroupColNote)
+                .AddRow(0, g[0].Title, g[0].Credit, g[0].Note)
+                .AddRow(1, g[1].Title, g[1].Credit, g[1].Note));
+        connection.Mocks
+            .When(cmd => cmd.CommandTextStartsWith(["SELECT"]) && cmd.SymbolAtContains(3, n.IpaCharacterTable))
+            .ReturnsTable(MockTable
+                .WithColumns(n.IpaCharacterColKey, n.IpaCharacterColSymbol, n.IpaCharacterColDiacritics)
+                .AddRow(0, "a", "")
+                .AddRow(1, "b", ":")
+                .AddRow(2, "c", ":;[+high]"));
+        connection.Mocks
+            .WhenAny()
+            .ReturnsTable(MockTable.WithColumns(n.PhonologicalRuleColKey, n.PhonologicalRuleColRule, n.PhonologicalRuleColRuleNote, n.PhonologicalRuleColGroupKey)
+                .AddRow(0, g[0].Rules[0].Rule, g[0].Rules[0].Note, 0)
+                .AddRow(1, g[0].Rules[1].Rule, g[0].Rules[1].Note, 0)
+                .AddRow(23, g[1].Rules[0].Rule, g[1].Rules[0].Note, 1)
+            );
+        DbReader reader = new();
+
+        var results = reader.FindRules(repo, new());
+
+        await Assert.That(results).Count().IsEqualTo(2);
+        foreach (var group in g)
+        {
+            Console.WriteLine(group);
+            await Assert.That(results).Contains(g => g.Title == group.Title
+                && g.Rules.Count == group.Rules.Count
+                && g.Rules.All(group.Rules.Contains));
+        }
+    }
+
+    [Test]
+    public async Task FindRules_EnvironmentFilter_UsedInSqlCode()
+    {
+        BetterMockDbConnection connection = new() { HasValidSqlServerCommandText = true };
+        var repo = IDbConnectionRepository.Mock();
+        repo.GetConnection<DbConnection>().Returns(connection);
+        IpaCharacter
+            charA = new("a", []),
+            charB = new("b", [":"]),
+            charC = new("c", [":", "[+high]"]);
+        List<PhonologicalRuleGroup> g =
+        [
+            new("English to French", "credit 1", [
+                new("a > b:", [charA], [charB], []),
+                new("a > c:[+high] / b:_", [charA], [charC], [charB]),
+            ]),
+            new("English to French", "credit2", Note: "NB: hi", Rules:[
+                new("ab > cc", [charA, charB], [charC], [], "note")
+            ])
+        ];
+        DbNames n = new();
+        CommandCapturer capturer = new();
+        connection.Mocks
+            .When(cmd => cmd.CommandTextStartsWith(["SELECT"]) && cmd.SymbolAtContains(3, n.RuleGroupTable))
+            .ReturnsTable(MockTable
+                .WithColumns(n.RuleGroupColKey, n.RuleGroupColName, n.RuleGroupColCredit, n.RuleGroupColNote)
+                .AddRow(0, g[0].Title, g[0].Credit, g[0].Note)
+                .AddRow(1, g[1].Title, g[1].Credit, g[1].Note));
+        connection.Mocks
+            .When(cmd => cmd.CommandTextStartsWith(["SELECT"]) && cmd.SymbolAtContains(3, n.IpaCharacterTable))
+            .ReturnsTable(MockTable
+                .WithColumns(n.IpaCharacterColKey, n.IpaCharacterColSymbol, n.IpaCharacterColDiacritics)
+                .AddRow(1, "b", ":"));
+        connection.Mocks
+            .When(capturer.Any)
+            .ReturnsTable(MockTable.WithColumns(n.PhonologicalRuleColKey, n.PhonologicalRuleColRule, n.PhonologicalRuleColRuleNote, n.PhonologicalRuleColGroupKey)
+                // .AddRow(0, g[0].Rules[0].Rule, g[0].Rules[0].Note, 0)
+                .AddRow(1, g[0].Rules[1].Rule, g[0].Rules[1].Note, 0)
+                .AddRow(23, g[1].Rules[0].Rule, g[1].Rules[0].Note, 1)
+            );
+        DbReader reader = new();
+        CharacterSearch search = new() { Character = "b", Environment = CharacterEnvironment.Input | CharacterEnvironment.Context};
+
+        var results = reader.FindRules(repo, search);
+
+        Debug.Assert(capturer.Count == 1);
+        await Assert.That(capturer[0]).Satisfies(cmd =>
+            cmd!.CommandText.Contains("input") && cmd.CommandText.Contains("context") && !cmd.CommandText.Contains("output")
+            || (cmd.Parameters.Any(p => p.Value as string == "input")
+                && cmd.Parameters.Any(p => p.Value as string == "context")
+                && cmd.Parameters.All(p => p.Value as string != "output")));
+        await Assert.That(results).Count().IsEqualTo(2);
+        foreach (var group in (PhonologicalRuleGroup[])[g[1], g[0] with {Rules = [g[0].Rules[1]]}])
+        {
+            Console.WriteLine(group);
+            await Assert.That(results).Contains(g => g.Title == group.Title
+                && g.Rules.Count == group.Rules.Count
+                && g.Rules.All(group.Rules.Contains));
+        }
+    }
+}
