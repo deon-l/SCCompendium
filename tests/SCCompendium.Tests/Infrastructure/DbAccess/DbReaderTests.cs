c7806bed163e7d61d9250bec1f2670e9536163e7
deon-l <deon.liu@outlook.com>
2026-07-04 17:24:35 -0400

    test: added test for verifying diacritics filter

--- tests/SCCompendium.Tests/Infrastructure/DbAccess/DbReaderTests.cs
+++ tests/SCCompendium.Tests/Infrastructure/DbAccess/DbReaderTests.cs
@@ -184,8 +184,8 @@ public class DbReaderTests
         Console.WriteLine("---");
         Console.WriteLine(capturer);
         Debug.Assert(capturer.Count == 1);
-        await Assert.That(miscCapturer).All().Satisfy(cmd => cmd.Satisfies(cmd => cmd.VerifyParameters()));
-        await Assert.That(capturer).All().Satisfy(cmd => cmd.Satisfies(cmd => cmd.VerifyParameters()));
+        await Assert.That(miscCapturer).All().Satisfy(cmd => cmd.Satisfies(trace => trace!.VerifyParameters()));
+        await Assert.That(capturer).All().Satisfy(cmd => cmd.Satisfies(trace => trace!.VerifyParameters()));
         await Assert.That(capturer[0]).Satisfies(cmd =>
             cmd!.CommandText.Contains("input") && cmd.CommandText.Contains("context") && !cmd.CommandText.Contains("output")
             || (cmd.Parameters.Any(p => p.Value as string == "input")
@@ -193,6 +193,67 @@ public class DbReaderTests
                 && cmd.Parameters.All(p => p.Value as string != "output")));
         await Assert.That(results).Count().IsEqualTo(2);
         foreach (var group in (PhonologicalRuleGroup[])[g[1], g[0] with {Rules = [g[0].Rules[1]]}])
+        {
+            Console.WriteLine(group);
+            await Assert.That(results).Contains(g => g.Title == group.Title
+                && g.Rules.Count == group.Rules.Count
+                && g.Rules.All(group.Rules.Select(StripReferenceChars).Contains));
+        }
+    }
+
+    [Test]
+    public async Task FindRules_DiacriticFilter_ValidSqlCode()
+    {
+        BetterMockDbConnection connection = new() { HasValidSqlServerCommandText = true };
+        var repo = IDbConnectionRepository.Mock();
+        repo.GetConnection<DbConnection>().Returns(connection);
+        IpaCharacter
+            charA = new("a", []),
+            charB = new("b", [":"]),
+            charC = new("c", [":", "[+high]"]);
+        List<PhonologicalRuleGroup> g =
+        [
+            new("English to French", "credit 1", [
+                new("a > b:", [charA], [charB], []),
+                new("a > c:[+high] / b:_", [charA], [charC], [charB]),
+            ]),
+            new("English to French", "credit2", Note: "NB: hi", Rules:[
+                new("ab > cc", [charA, charB], [charC], [], "note")
+            ])
+        ];
+        DbNames n = new();
+        CommandCapturer miscCapturer = new();
+        CommandCapturer capturer = new();
+        connection.Mocks
+            .When(miscCapturer.If(cmd => cmd.CommandTextStartsWith(["SELECT"]) && cmd.SymbolAtContains(3, n.RuleGroupTable)))
+            .ReturnsTable(MockTable
+                .WithColumns(n.RuleGroupColKey, n.RuleGroupColName, n.RuleGroupColCredit, n.RuleGroupColNote)
+                .AddRow(0, g[0].Title, g[0].Credit, g[0].Note)
+                .AddRow(1, g[1].Title, g[1].Credit, g[1].Note));
+        connection.Mocks
+            .When(capturer.If(cmd => cmd.CommandTextStartsWith(["SELECT"]) && cmd.SymbolAtContains(3, n.IpaCharacterTable)))
+            .ReturnsTable(MockTable
+                .WithColumns(n.IpaCharacterColKey, n.IpaCharacterColSymbol, n.IpaCharacterColDiacritics)
+                .AddRow(1, "b", ":"));
+        connection.Mocks
+            .When(miscCapturer.Any)
+            .ReturnsTable(MockTable.WithColumns(n.PhonologicalRuleColKey, n.PhonologicalRuleColRule, n.PhonologicalRuleColRuleNote, n.PhonologicalRuleColGroupKey)
+                .AddRow(0, g[0].Rules[0].Rule, g[0].Rules[0].Note, 0)
+                .AddRow(1, g[0].Rules[1].Rule, g[0].Rules[1].Note, 0)
+                .AddRow(23, g[1].Rules[0].Rule, g[1].Rules[0].Note, 1)
+            );
+        DbReader reader = new();
+        CharacterSearch search = new() { Diacritics = [":", "[+high]"]};
+
+        var results = reader.FindRules(repo, search);
+
+        Console.WriteLine(miscCapturer);
+        Console.WriteLine("---");
+        Console.WriteLine(capturer);
+        await Assert.That(miscCapturer).All().Satisfy(cmd => cmd.Satisfies(trace => trace!.VerifyParameters()));
+        await Assert.That(capturer).All().Satisfy(cmd => cmd.Satisfies(trace => trace!.VerifyParameters()));
+        await Assert.That(results).Count().IsEqualTo(2);
+        foreach (var group in g)
         {
             Console.WriteLine(group);
             await Assert.That(results).Contains(g => g.Title == group.Title
