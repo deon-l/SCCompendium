ba0da20f094e8a135671effcf32039a311cbb679
deon-l <deon.liu@outlook.com>
2026-07-07 15:01:59 -0400

    feat: Added `AppParse`

--- src/SCCompendium/Presentation/App.cs
+++ src/SCCompendium/Presentation/App.cs
@@ -1,6 +1,16 @@
+using CommandDotNet;
+using SCCompendium.Application.DbAccess;
+using SCCompendium.Application.Parser;
+
 namespace SCCompendium.Tests.Presentation;
 
 public class App
 {
+    public static IDbConnectionRepository DbConnectionRepository { get; set; } = null!;
+    public static IDiachronicaParser DiachronicaParser { get; set; } = null!;
+    public static IDbReader DbReader { get; set; } = null!;
+    public static IDbWriter DbWriter { get; set; } = null!;
 
+    [Subcommand]
+    public AppParse Parse { get; set; } = null!;
 }
---
+++ src/SCCompendium/Presentation/AppParse.cs
@@ -0,0 +1,21 @@
+using CommandDotNet;
+using SCCompendium.Domain.ValueObjects.Parsed;
+
+namespace SCCompendium.Tests.Presentation;
+
+[Subcommand]
+public class AppParse
+{
+    [DefaultCommand]
+    [Command(Description = "Parses the specified file phonological rules and uploads it to the SQL database specified by the connection string.")]
+    public void All(
+        [Operand]string fileName,
+        [Option('c', "connection")]string connectionString)
+    {
+        App.DbConnectionRepository.CreateConnection(connectionString);
+        StreamReader file = new(File.OpenRead(fileName));
+
+        List<PhonologicalRuleGroup> rules = App.DiachronicaParser.Parse(file);
+        App.DbWriter.Write(App.DbConnectionRepository, rules);
+    }
+}
