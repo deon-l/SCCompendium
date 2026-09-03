d854478ddd50354056a0a5c3cb9471e12ee57065
deon-l <deon.liu@outlook.com>
2026-09-03 17:57:21 -0400

    doc: Added documentation to `PrintOptions` Argument Model.

--- src/SCCompendium/Presentation/App/ArgumentModels/PrintOptions.cs
+++ src/SCCompendium/Presentation/App/ArgumentModels/PrintOptions.cs
@@ -1,4 +1,5 @@
 using CommandDotNet;
+using SCCompendium.Domain.ValueObjects.Parsed;
 
 namespace SCCompendium.Presentation.App.ArgumentModels;
 
@@ -7,17 +8,22 @@ namespace SCCompendium.Presentation.App.ArgumentModels;
 /// </summary>
 public record struct PrintOptions : IArgumentModel
 {
+    /// <summary>Whether other data of <see cref="PhonologicalRuleGroup"/> (e.g. titles, credit) should be printed.</summary>
     [Option('g', "print-groups")]
     public bool PrintGroups { get; set; }
 
+    /// <summary>Whether the string representation of the <see cref="PhonologicalRule"/>s should be printed.</summary>
     [Option('r', "print-rules")]
     public bool PrintRules { get; set; }
 
+    /// <summary>Whether to print all characters used in a collection of rules</summary>
     [Option('c', "print-chars")]
     public bool PrintCharacters { get; set; }
 
+    /// <summary>Whether to print all diacritics/modifiers found in the collection of rules.</summary>
     [Option('d', "print-diacritics")]
     public bool PrintDiacritics { get; set; }
 
+    /// <remarks>i.e. every member is <see langword="false"/></remarks>
     public bool NoOptionsSelected() => this == default;
 }
