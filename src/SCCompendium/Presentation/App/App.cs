3fdfa061c0ae95d3c374c1028a7750431459777c
deon-l <deon.liu@outlook.com>
2026-09-02 23:14:31 -0400

    refactor: Moved parse back as just a command of App

--- src/SCCompendium/Presentation/App/App.cs
+++ src/SCCompendium/Presentation/App/App.cs
@@ -3,13 +3,23 @@ using CommandDotNet;
 namespace SCCompendium.Presentation.App;
 
 /// <remarks>
-/// Bass command.
+/// Base command.
 /// </remarks>
 [Command(Description = "Utility to parse Phonological data, upload to MySQL databases, and read from them.")]
 public class App
 {
-    [Subcommand(RenameAs = "parse")]
-    public AppParse Parse { get; set; } = null!;
+    [Command(Description = "Parses the specified file for latex phonological rules and redirects it elsewhere.")]
+    public void Parse(
+        [Operand]string fileName,
+        [Option('c', "connection")]string connectionString,
+        [Option('r', "print-rules")]bool print-rules)
+    {
+        connectionRepo.CreateConnection(connectionString);
+        StreamReader file = new(File.OpenRead(fileName));
+
+        List<PhonologicalRuleGroup> rules = diaParser.Parse(file);
+        dbWriter.Write(connectionRepo, rules);
+    }
     [Subcommand(RenameAs = "search")]
     public AppSearch Search { get; set; } = null!;
 }
