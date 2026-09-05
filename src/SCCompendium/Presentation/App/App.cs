e395e94c69b1076be9736515629d2a349fcb46c2
deon-l <deon.liu@outlook.com>
2026-09-05 00:43:21 -0400

    feat: Added new dependencies.

--- src/SCCompendium/Presentation/App/App.cs
+++ src/SCCompendium/Presentation/App/App.cs
@@ -9,6 +9,11 @@ namespace SCCompendium.Presentation.App;
 [Command(Description = "Utility to parse Phonological data, upload to MySQL databases, and read from them.")]
 public class App(AppParse parseCommand)
 {
+    public static IEnumerable<Type> GetCommandImplementationDependencies()
+    {
+        yield return typeof(AppParse);
+    }
+
     [Command(Description = "Parses the specified file for latex phonological rules and redirects it elsewhere.")]
     public void Parse(
         [Operand]string fileName,
