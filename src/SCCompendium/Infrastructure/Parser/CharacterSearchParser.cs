3667653c32c645825be3c073e81282ff5b4ace1b
deon-l <deon.liu@outlook.com>
2026-09-10 12:06:46 -0400

    doc: Updated documentation.

---
+++ src/SCCompendium/Infrastructure/Parser/CharacterSearchParser.cs
@@ -0,0 +1,13 @@
+using SCCompendium.Application.Parser;
+using SCCompendium.Tests.Domain.ValueObjects.DbValues;
+
+namespace SCCompendium.Infrastructure.Parser;
+
+/// <inheritdoc cref="ICharacterSearchParser"/>
+public class CharacterSearchParser : ICharacterSearchParser
+{
+    public CharacterSearch GetCharacterSearch(string? input)
+    {
+        throw new NotImplementedException();
+    }
+}
